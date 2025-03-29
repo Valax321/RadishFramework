942cb0e45c25cdbf7573e7bf3226f1f61e9e92a9
Audrey Castillo <valax321@gmail.com>
2025-03-29 12:42:53 +1100

    Add imgui module

---
+++ Source/Radish.UI/ImGuiManager.cs
@@ -0,0 +1,51 @@
+﻿using ImGuiNET;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Radish.Framework;
+using Radish.Graphics;
+using Radish.Services;
+
+namespace Radish.UI;
+
+[PublicAPI]
+public sealed class ImGuiManager : 
+    IDisposable, 
+    IGameDraw, 
+    IGameUpdate,
+    IServiceConsumer
+{
+    // Needs to update FIRST and draw LAST
+    public int DrawOrder => int.MaxValue;
+    public int UpdateOrder => int.MinValue;
+
+    private IntPtr _context;
+    private IGraphicsDevice _graphicsDevice = null!;
+
+    public ImGuiManager(GameServiceCollection services)
+    {
+        services.AddSingleton(this);
+        
+        _context = ImGui.CreateContext();
+    }
+    
+    public void ResolveServices(IServiceProvider services)
+    {
+        _graphicsDevice = services.GetRequiredService<IGraphicsDevice>();
+    }
+    
+    public void Update(TimeSpan deltaTime)
+    {
+        ImGui.NewFrame();
+    }
+    
+    public void Draw(TimeSpan deltaTime)
+    {
+        ImGui.Render();
+    }
+    
+    public void Dispose()
+    {
+        ImGui.DestroyContext(_context);
+        _context = IntPtr.Zero;
+    }
+}
--- TestGame/TestApplication.cs
+++ TestGame/TestApplication.cs
@@ -1,6 +1,7 @@
 using Radish.Filesystem;
 using Radish.Framework;
 using Radish.Logging;
+using Radish.UI;
 
 namespace Radish.TestGame;
 
@@ -9,11 +10,14 @@ public class TestApplication : Application
     private static readonly ILogger Logger = LogManager.GetLoggerForType<TestApplication>();
     
     public ResourceManager ResourceManager { get; }
+    public ImGuiManager ImGuiManager { get; }
     
     public TestApplication(in ApplicationOptions options) : base(in options)
     {
         ResourceManager = new ResourceManager(ServiceCollection,
             new ResourceManagerOptions(Platform.GetBasePath()));
+        
+        ImGuiManager = new ImGuiManager(ServiceCollection);
     }
 
     protected override void Initialize()
